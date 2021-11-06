c170004f3d670c49bd0d1ad778f75969ce1f3a76
Sosuke Suzuki <aosukeke@gmail.com>
2021-11-06 18:34:27 +0900

    Import zxcvbn

--- src/pages/happy.js
+++ src/pages/happy.js
@@ -1,3 +1,9 @@
+import { useEffect } from "react";
+import zxcvbn from "zxcvbn";
+
 export default function () {
+  useEffect(() => {
+    console.log(zxcvbn);
+  }, []);
   return <h1>Happy Website</h1>;
 }
